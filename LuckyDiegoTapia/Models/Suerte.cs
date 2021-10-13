bb5e94601cbedae0f86e650b1305498c3c127ab0
Diego <a2018118018@estudiantes.upsa.edu.bo>
2021-10-13 19:34:34 -0400

    2do Commit creacion Model y clase

---
+++ LuckyDiegoTapia/Models/Suerte.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPeople2.Models
+{
+    public enum DetalleType
+    {
+        Malasuerte = 0,
+        Buenasuerte = 1,
+    }
+    public class Person
+    {
+        [Key]
+        public int SuerteId { get; set; }
+        [StringLength(10, MinimumLength = 1, ErrorMessage = "contiene del 1 al 10")]
+        
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Quien consulta su suerte?")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Detalle buena o mala suerte que le llegará")]
+        public DetalleType Detalle { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Imagen { get; set; }
+
+    }
+}
